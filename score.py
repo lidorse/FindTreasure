92972eafba430772f2406b118cf8bbd88c973f88
lidorse <97695541+lidorse@users.noreply.github.com>
2022-01-31 23:57:11 +0200

    Update score.py

--- score.py
+++ score.py
@@ -1,14 +1,14 @@
 with open(r'C:\Users\lidor\Desktop\hodi\Score.txt','r+') as score:
     listOfScore = []
-    num = 8
-    for small in score.readlines(): #check all the number that smaller from num. of tries. Then appended to listOfScore
-        if int(small) < num:
+    numOftries = len(tries)
+    for small in score.readlines(): #check all the numbers that smaller from numOftries Then appended to listOfScore
+        if int(small) < numOftries:
             listOfScore.append(small)
-    listOfScore.append(str(num) + '\n') #append the num. of tries to the listOfScore
+    listOfScore.append(str(numOftries) + '\n') #append the player's numOftries to the listOfScore
     score.seek(0)
-    for big in score.readlines(): #check all the number that big from num. of tries. Then appended to listOfScore
-        if int(big) > num:
+    for big in score.readlines(): #check all the number that big from numOftries Then appended to listOfScore
+        if int(big) > numOftries:
             listOfScore.append(big)
 
-with open(r'C:\Users\lidor\Desktop\hodi\Score.txt', 'w') as score: #open file to overwrite the list
+with open(r'C:\Users\lidor\Desktop\hodi\Score.txt', 'w') as score: #open file to overwrite the listOfScore
     score.writelines(listOfScore[:-1]) #overwrite the file and remove the last score so there will be 10 places
